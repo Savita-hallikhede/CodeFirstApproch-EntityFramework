622a1fa651083cba5ae9add176c251ad855c3fe0
Savita-hallikhede <savitahallikhede11@gmail.com>
2026-03-29 18:08:43 +0530

    Add files via upload

---
+++ Models/Register.cs
@@ -0,0 +1,19 @@
+﻿namespace code_first_approch.Models
+{
+    public class Register
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+
+        public string gender { get; set; }
+
+        public string Address { get; set; }
+
+        public long PhoneNumber { get; set; }
+
+        public DateTime DateOfBirth { get; set; }
+
+    }
+}
